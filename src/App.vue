7dcf91d2231ae8ff4bacd4a541006ba47e6eea20
90090611099 <swiderski.p@gmail.com>
2019-04-02 16:06:40 +0200

    Add existing file

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,22 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+     <NavBar/>
     </div>
     <router-view />
   </div>
 </template>
+
+<script>
+import NavBar from './components/NavBar'
+export default {
+ components: {
+   NavBar
+ } 
+}
+</script>
+
+
 
 <style lang="scss">
 #app {
@@ -17,7 +27,6 @@
   color: #2c3e50;
 }
 #nav {
-  padding: 30px;
   a {
     font-weight: bold;
     color: #2c3e50;
---
+++ src/components/NavBar.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="navbar">
+        <router-link :to="{ name: 'home' }">Home</router-link> |
+        <router-link :to="{ name: 'blog' }">Blog</router-link> |
+        <router-link :to="{ name: 'store'}">Store</router-link>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'NavBar'
+}
+</script>
+
+<style lang="scss" scoped>
+#navbar { width:100%; height: 30px; background-color:deeppink;
+a {
+    vertical-align: center
+}}
+</style>
